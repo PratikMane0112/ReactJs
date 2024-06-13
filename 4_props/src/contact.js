51331ae71ad66849181763c86e80104fb5903d5b
PratikMane@0112 <pratikmane610@gmail.com>
2024-06-14 03:29:19 +0530

    Add props

---
+++ 4_props/src/contact.js
@@ -0,0 +1,25 @@
+const contacts = [
+    {
+      name: "Harry",
+      imgURL:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAMFYLFQ2DS0BbP86UMTz16eMLbSwzl91F_AEmCYAh3zKegYqqOt5xzkmPZ1mbcS9qxd0&usqp=CAU",
+      phone: "+123 456 789",
+      email: "magic@harry.com"
+    },
+    {
+      name: "Hermoine",
+      imgURL:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJLSjehR9BF2aLed_fVqBhy4ZK-8f9HK0GYQ&s",
+      phone: "+987 654 321",
+      email: "magic@hermoie.com"
+    },
+    {
+      name: "Ron",
+      imgURL:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfpYibRpRa_jrzA4TX_LbVKsFDzzQMd4Bgcw&s",
+      phone: "+918 372 574",
+      email: "magic@ronaldcom"
+    }
+  ];
+
+  export default contacts;
